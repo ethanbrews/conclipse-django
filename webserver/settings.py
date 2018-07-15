bd0e8b11f1106b5ff2dfc96f3f819edeb1f9828a
Ethan Brews <ethanbrews77@gmail.com>
2018-07-15 19:42:29 +0100

    safe for online use

--- webserver/settings.py
+++ webserver/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o8rmn&)-x(*uc^rn5*g4#yqpj#+9gky@_+g34fb)t-2v9*u%*v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
